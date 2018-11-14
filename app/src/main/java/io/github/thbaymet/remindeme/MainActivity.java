b9d5921aea37e622e01259a3984c19f6e5e82cdb
BAYRAMOV Matin <bayramov.matin@gmail.com>
2018-11-14 04:50:57 +0100

    Added simple dialog alert.

--- app/src/main/java/io/github/thbaymet/remindeme/MainActivity.java
+++ app/src/main/java/io/github/thbaymet/remindeme/MainActivity.java
@@ -1,7 +1,8 @@
 package io.github.thbaymet.remindeme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(R.string.app_name);
+        alertDialogBuilder.setMessage("The only way to get smarter is by playing a smarter opponent.");
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
